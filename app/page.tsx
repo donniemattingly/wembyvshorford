bc8605dc398d88c79cfef4bd8a065c3199aafc57
Chad Orwig <chad.orwig@slalom.com>
2023-11-11 13:16:35 -0500

    add timing breakdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,78 +1,90 @@
-import Image from 'next/image'
+import React from 'react';
 
 import { JSDOM } from 'jsdom';
 import { strict as assert } from 'assert';
+import TimingBreakdown from '@/components/TimingBreakdown';
 
-const cacheTime = 60 * 60 * 8 // 8 hours (in seconds)
-const fetchParams = { next: { revalidate: cacheTime }};
-
-const getWinShareFromHtml = (html: string): string => {
-    const dom = new JSDOM(html);
-    const document = dom.window.document;
-    let winSharesTipElement = document.querySelector('span.poptip[data-tip*="Win Shares"]');
-    const winShares = winSharesTipElement?.parentElement?.lastChild?.textContent;
-    assert(winShares != null, "Couldn't find win shares");
-    return winShares;
-}
 
 const horfordUrl = 'https://www.basketball-reference.com/players/h/horfoal01.html';
 const wembyUrl = 'https://www.basketball-reference.com/players/w/wembavi01.html'
+const cacheTime = 60 * 60 * 8 // 8 hours (in seconds)
+const fetchParams = { next: { revalidate: cacheTime } };
+type PopTip = 'Win Shares' | 'Games';
+
+const readPopTip = (document: Document, dataTip: PopTip, exact: boolean): number => {
+  const query = `span.poptip[data-tip${exact ? '' : '*' }="${dataTip}"]`;
+  const element = document.querySelector(query);
+  const value = element?.parentElement?.lastChild?.textContent;
+  assert(value != null, `Couldn't find ${dataTip}`);
+  return parseFloat(value);
+}
+
+const getDataFromHtml = (html: string) => {
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+  const data = {
+    winShares: readPopTip(document, 'Win Shares', false),
+    games: readPopTip(document, 'Games', true),
+  };
+  return data;
+}
 const fetchData = async () => {
 
-    try {
-        // Fetch both URLs in parallel
-        const [wembyResponse, horfordResponse] = await Promise.all([
-            fetch(wembyUrl, fetchParams),
-            fetch(horfordUrl, fetchParams)
-        ]);
+  try {
+    // Fetch both URLs in parallel
+    const [wembyResponse, horfordResponse] = await Promise.all([
+      fetch(wembyUrl, fetchParams),
+      fetch(horfordUrl, fetchParams)
+    ]);
 
-        // Parse the JSON response
-        const wembyHtml = await wembyResponse.text();
-        const horfordHtml = await horfordResponse.text();
+    // Parse the JSON response
+    const wembyHtml = await wembyResponse.text();
+    const horfordHtml = await horfordResponse.text();
 
-        const wembyShares = getWinShareFromHtml(wembyHtml);
-        const horfordShares = getWinShareFromHtml(horfordHtml)
+    const wemby = getDataFromHtml(wembyHtml);
+    const horford = getDataFromHtml(horfordHtml)
 
-        return {
-            wemby: parseFloat(wembyShares),
-            horford: parseFloat(horfordShares)
-        }
+    return {
+      wemby,
+      horford,
+    };
 
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 
-    return null;
+  return null;
 }
 
 export default async function Home() {
-    const data = await fetchData();
-    if (data !== null) {
-        const {wemby, horford} = data;
-        return (
-            <>
-                <main className="flex min-h-screen flex-col items-center justify-center p-24">
-                    <div>
-                        Does Victor Wembanyama have more career win shares than Al Horford?
-                    </div>
-                    <div className='text-9xl font-extrabold pt-24'>
-                        {horford > wemby
-                            ? <h1> No </h1>
-                            : <h1> Yes </h1>
-                        }
-                    </div>
-                    <div
-                        className="flex flex-row text-lg w-1/2 justify-between font-normal pt-24 underline text-cyan-400">
-                    <span>
-                        <a href={horfordUrl}> Horford: {horford} </a>
-                    </span>
-                        <span>
-                        <a href={wembyUrl}> Wembanyama: {wemby} </a>
-                    </span>
-                    </div>
-                </main>
-                <footer> Proudly built with Next.js</footer>
-            </>
-        )
-    }
+  const data = await fetchData();
+  if (data !== null) {
+    const { wemby, horford } = data;
+    return (
+      <>
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+          <div>
+            Does Victor Wembanyama have more career win shares than Al Horford?
+          </div>
+          <div className='text-9xl font-extrabold pt-24'>
+            {horford.winShares > wemby.winShares
+              ? <h1> No </h1>
+              : <h1> Yes </h1>
+            }
+          </div>
+          <div
+            className="flex flex-row text-lg w-1/2 justify-between font-normal pt-24 underline text-cyan-400">
+            <span>
+              <a href={horfordUrl}> Horford: {horford.winShares} </a>
+            </span>
+            <span>
+              <a href={wembyUrl}> Wembanyama: {wemby.winShares} </a>
+            </span>
+          </div>
+          <TimingBreakdown pronoun="him" winShares={wemby.winShares} games={wemby.games} target={horford.winShares} />
+        </main>
+        <footer> Proudly built with Next.js</footer>
+      </>
+    )
+  }
 }
---
+++ components/TimingBreakdown.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const gamesPerSeason = 82 as const;
+
+export type TimingBreakdownPropTypes = {
+  pronoun: string;
+  winShares: number;
+  games: number;
+  target: number;
+}
+
+function TimingBreakdown({ pronoun, winShares, games, target}: TimingBreakdownPropTypes) {
+  if(winShares > target) {
+    return null;
+  }
+  const rate = winShares/games;
+  const remainingGames = (target - winShares) / rate;
+  const remainingGamesString = remainingGames.toLocaleString(undefined, {maximumFractionDigits: 0 });
+  const remainingSeasons = remainingGames / gamesPerSeason;
+  const remainingSeasonsString = remainingSeasons.toLocaleString(undefined, { maximumFractionDigits: 1 });
+  
+  return (
+    <p className='p-4'>
+      At this rate it will take {pronoun} {remainingGamesString} more games
+      or {remainingSeasonsString} complete seasons (Assuming he plays
+      all {gamesPerSeason} games).
+    </p>
+  )
+}
+
+export default TimingBreakdown;
